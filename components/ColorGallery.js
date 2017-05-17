cc4e47731d7e5fa910c25865c845c154f0e82cf5
Marcos Mellado <marcos@mellados.com>
2017-05-17 22:31:15 +0200

    Refactored code to use a variable instead of hardcoded color

--- components/ColorGallery.js
+++ components/ColorGallery.js
@@ -4,7 +4,7 @@ export default class ColorGallery extends React.Component {
   render() {
     return (
       <div className="cg">
-        <div className="cg-active-content" style={{background: 'blue'}} />
+        <div className="cg-active-content" style={{background: this.props.colors[0]}} />
         <ul>
           {
             this.props.colors.map(color => <li className="cg-option" key={color} style={{background: color}} />)
