42b81e1a0238921b9b560a30a8b078cbc51f201e
Marcos Mellado <marcos@mellados.com>
2017-05-17 22:34:49 +0200

    Updated code for tests to pass -- refactored code ensuring all tests still pass

--- components/ColorGallery.js
+++ components/ColorGallery.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 export default class ColorGallery extends React.Component {
+  state = {
+    active: this.props.colors[0],
+  }
   render() {
     return (
       <div className="cg">
-        <div className="cg-active-content" style={{background: this.props.colors[0]}} />
+        <div className="cg-active-content" style={{background: this.state.active}} />
         <ul>
           {
             this.props.colors.map(color => <li className="cg-option" key={color} style={{background: color}} />)
