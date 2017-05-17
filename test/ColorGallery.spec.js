59832420080f07ef1946e58f2065fa6059412ffb
Marcos Mellado <marcos@mellados.com>
2017-05-17 22:13:37 +0200

    Adding second test

--- test/ColorGallery.spec.js
+++ test/ColorGallery.spec.js
@@ -8,6 +8,13 @@ describe('<ColorGallery />', () => {
     );
     expect(wrapper.find('.cg-active-content')).to.have.tagName('div');
   });
+
+  it('should create an li for every color', () => {
+    const wrapper = render(
+      <ColorGallery colors={['blue', 'green', 'red', 'yellow']} />
+    );
+    expect(wrapper.find('li.cg-option')).to.have.length(4);
+  });
 });
 
 
