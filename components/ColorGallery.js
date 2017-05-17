ccae6f54f91fb8bd7f1916451a68853893899f1a
Marcos Mellado <marcos@mellados.com>
2017-05-17 22:12:57 +0200

    Adding component code to pass first test

---
+++ components/ColorGallery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ColorGallery extends React.Component {
+  render() {
+    return (
+      <div className="cg">
+        <div className="cg-active-content" />
+      </div>
+    );
+  }
+}
